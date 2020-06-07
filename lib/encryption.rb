45bb4ce93aed419e0152a242938e384ec31e9da6
Lito White <leland.white@gmail.com>
2020-06-07 14:33:06 -0600

    Limited Funcitonality Complete

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -9,4 +9,24 @@ module Encryption
     arr
   end
 
+  def create_offset(date)
+   ((date.to_i ** 2).to_s[-4..-1]).split("").map {|num| num.to_i}
+  end
+
+  def final_shift(key, offset)
+    key.map.with_index { |v,i| v + offset[i] }
+  end
+
+  def encrypt(message,final_shift)
+    character_set = ("a".."z").to_a << " "
+    split_message = message.split("")
+    offset_array = split_message.map.with_index do |char, index|
+      offset = final_shift[index % 4]
+      original = character_set.find_index(char)
+      new_char_index = (original + offset) % 27
+      character_set[new_char_index]
+    end
+    offset_array.join
+  end
+
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -8,4 +8,21 @@ class EncryptionTest < Minitest::Test
     assert_equal [2,27,71,15], actual
   end
 
+  def test_it_can_create_offset
+    actual = Encryption.create_offset("040895")
+    assert_equal [1,0,2,5], actual
+  end
+
+  def test_final_shift
+    actual = Encryption.final_shift([2,27,71,15], [1,0,2,5])
+    expected = [3,27,73,20]
+    assert_equal expected, actual
+  end
+
+  def test_it_can_encrypt
+    actual = Encryption.encrypt("hello world", [3,27,73,20])
+    expected =  "keder ohulw"
+    assert_equal expected, actual
+  end
+
 end
